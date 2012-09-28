b7c2b6e6b569416d44129df9d572c26a390b29d5
David Munn <munnday@gmail.com>
2012-09-28 10:07:46 +1000

    Added support for Custom Backgrounds

--- functions.php
+++ functions.php
@@ -16,18 +16,23 @@ Custom functions, support, custom post types and more.
    Theme Support
    ========================================================================== */
    
-   if ( function_exists( 'add_theme_support' ) ) {
+	if ( function_exists( 'add_theme_support' ) ) {
    	
-   	add_theme_support('menus'); // Add Menu Support
+		add_theme_support('menus'); // Add Menu Support
 	
-	add_theme_support('post-thumbnails'); // Add Thumbnail Theme Support
-	add_image_size( 'large', 700, '', true ); // Large Thumbnail
-	add_image_size( 'medium', 250, '', true ); // Medium Thumbnail
-	add_image_size( 'small', 120, '', true ); // Small Thumbnail
-	add_image_size( 'custom-size', 700, 200, true ); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
+		add_theme_support('post-thumbnails'); // Add Thumbnail Theme Support
+		add_image_size( 'large', 700, '', true ); // Large Thumbnail
+		add_image_size( 'medium', 250, '', true ); // Medium Thumbnail
+		add_image_size( 'small', 120, '', true ); // Small Thumbnail
+		add_image_size( 'custom-size', 700, 200, true ); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
 	
+		// Add Support for Custom Backgrounds (http://codex.wordpress.org/Custom_Backgrounds)
+		add_theme_support( 'custom-background', array(
+			'default-color' => $default_background_color,
+			)
+		);
    }
-   
+
 /* =============================================================================
    Functions
    ========================================================================== */
