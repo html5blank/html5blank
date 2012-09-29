731faad623db0e6b6c9da0c21a50f729b70b820e
Todd Motto <create@toddmotto.com>
2012-09-29 01:17:19 +0100

    Favicon Support in header.php with directory URL to theme folder

--- header.php
+++ header.php
@@ -10,6 +10,8 @@
 	<!-- Meta -->
 	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0;">
 	<meta name="description" content="<?php bloginfo('description'); ?>">
+	
+	<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/img/favicon.ico">
 		
 	<!-- CSS + jQuery + JavaScript -->
 	<?php wp_head(); ?>
