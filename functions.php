ca372b247c2a701648b6c1f9f850675bc8ecf5f4
toddmotto <create@toddmotto.com>
2012-10-02 12:35:31 +0200

    Filter added to allow Excerpt Shortcodes

--- functions.php
+++ functions.php
@@ -259,16 +259,18 @@ Custom functions, support, custom post types and more.
    remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
    
    // Filters
-   add_filter( 'body_class', 'add_slug_to_body_class' ); // Add slug to body class (Starkers build)
+   add_filter('body_class', 'add_slug_to_body_class'); // Add slug to body class (Starkers build)
    add_filter('widget_text', 'do_shortcode'); // Allow shortcodes in Dynamic Sidebar
    add_filter('widget_text', 'shortcode_unautop'); // Remove <p> tags in Dynamic Sidebars (better!)
-   add_filter( 'wp_nav_menu_args', 'my_wp_nav_menu_args' ); // Remove surrounding <div> from WP Navigation
+   add_filter('wp_nav_menu_args', 'my_wp_nav_menu_args'); // Remove surrounding <div> from WP Navigation
    add_filter('nav_menu_css_class', 'my_css_attributes_filter', 100, 1); // Remove Navigation <li> injected classes
    add_filter('nav_menu_item_id', 'my_css_attributes_filter', 100, 1); // Remove Navigation <li> injected ID
    add_filter('page_css_class', 'my_css_attributes_filter', 100, 1); // Remove Navigation <li> Page ID's
    add_filter('the_category', 'remove_category_rel_from_category_list'); // Remove invalid rel attribute
+   add_filter('the_excerpt', 'shortcode_unautop'); // Remove auto <p> tags in Excerpt (Manual Excerpts only)
+   add_filter('the_excerpt', 'do_shortcode'); // Allows Shortcodes to be executed in Excerpt (Manual Excerpts only)
    add_filter('excerpt_more', 'html5wp_view_article'); // Add 'View Article' button instead of [...] for Excerpts
-   add_filter( 'show_admin_bar' , 'remove_admin_bar'); // Remove Admin bar
+   add_filter('show_admin_bar' , 'remove_admin_bar'); // Remove Admin bar
    add_filter('style_loader_tag', 'html5_style_remove'); // Remove 'text/css' from enqueued stylesheet
    
    // Shortcodes
