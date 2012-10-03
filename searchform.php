39ae1404739853368ae4fba65f5883d844a17263
David Munn <munnday@gmail.com>
2012-10-03 23:37:24 +1000

    home_url used instead of bloginfo('home)

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <!-- Searchform -->
-<form method="get" class="search" action="<?php bloginfo('home'); ?>" >
+<form method="get" class="search" action="<?php echo home_url(); ?>" >
 	<input id="s" type="text" name="s" onfocus="if(this.value==''){this.value=''};" 
 	onblur="if(this.value==''){this.value=''};" value="">
 	<input class="searchsubmit" type="submit" value="<?php _e( 'Search', 'html5blank' ); ?>">
