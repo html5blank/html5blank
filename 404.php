bec6d37269284922ab885d2fdf67847e41ae5445
David Munn <munnday@gmail.com>
2012-10-03 23:41:54 +1000

    home_url instead of bloginfo('home')

--- 404.php
+++ 404.php
@@ -7,7 +7,7 @@
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	
 		<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
-		<h2><a href="<?php bloginfo('url'); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a></h2>
+		<h2><a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a></h2>
 		
 	</article>
 	<!-- /Article -->
