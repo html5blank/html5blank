426a719bc189eea49bfae39dc5297ea98e1f1576
Todd Motto <create@toddmotto.com>
2012-10-03 16:39:05 +0100

    Footer Copyright Name + Credit

--- footer.php
+++ footer.php
@@ -1,6 +1,13 @@
 	<!-- Footer -->
 	<footer>
-		&copy; <?php echo date("Y"); ?> Copyright.
+		
+		<!-- Copyright -->
+		<p class="copyright">
+			&copy; <?php echo date("Y"); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?> 
+			<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
+		</p>
+		<!-- /Copyright -->
+		
 	</footer>
 	<!-- /Footer -->
 	
