f58bf7bf9ac5fbc248cfb167df4e6a4cc4f9dc90
Todd Motto <create@toddmotto.com>
2012-10-07 21:57:15 +0100

    Apple Mobile Web App capable meta tag, for Web App full screen.

--- header.php
+++ header.php
@@ -9,6 +9,9 @@
 	
 	<!-- Meta -->
 	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0;">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	
 	<meta name="description" content="<?php bloginfo('description'); ?>">
 	
 	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
