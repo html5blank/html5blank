4d3b6b3943464495c938922ce565d93cfbc561f8
Todd Motto <create@toddmotto.com>
2012-10-07 21:57:42 +0100

    Hidden URL bar for iPhone/iPad after page loads, for responsive project benefit

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
 // DOM Ready
 $(document).ready(function() {
 	// jQuery Code
+	
+	
+	
+	
+	// Responsive Projects, iPhone/iPad URL bar hides itself on pageload
+	if (navigator.userAgent.indexOf('iPhone') != -1) {
+	    addEventListener("load", function () {
+	        setTimeout(hideURLbar, 0);
+	    }, false);
+	}
+	
+	function hideURLbar() {
+	    window.scrollTo(0, 0);
+	}
 });
