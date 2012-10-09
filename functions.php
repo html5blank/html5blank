0f040a15ac37837de3e37d896d3dd9735e6ce0a9
toddmotto <create@toddmotto.com>
2012-10-09 22:22:03 +0200

    Added support for Custom Header with small addition to Custom Backgrounds

--- functions.php
+++ functions.php
@@ -38,8 +38,23 @@ if (function_exists('add_theme_support'))
 
     // Add Support for Custom Backgrounds
     add_theme_support('custom-background', array(
-        'default-color' => $default_background_color
+	'default-color' => 'FFF',
+	'default-image' => get_template_directory_uri() . '/img/bg.jpg'
     ));
+
+    // Add Support for Custom Header
+    add_theme_support('custom-header', array(
+	'default-image'			=> get_template_directory_uri() . '/img/headers/default.jpg',
+	'header-text'			=> false,
+	'default-text-color'		=> '000',
+	'width'				=> 1000,
+	'height'			=> 198,
+	'random-default'		=> false,
+	'wp-head-callback'		=> $wphead_cb,
+	'admin-head-callback'		=> $adminhead_cb,
+	'admin-preview-callback'	=> $adminpreview_cb
+    ));
+
     // Enables post and comment RSS feed links to head
     add_theme_support('automatic-feed-links');
 
