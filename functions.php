4c34bacde3db0e6db367e99f7eb3c05a8aef61cb
toddmotto <create@toddmotto.com>
2012-10-10 22:07:51 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -36,14 +36,14 @@ if (function_exists('add_theme_support'))
     add_image_size('small', 120, '', true); // Small Thumbnail
     add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
 
-    // Add Support for Custom Backgrounds
-    add_theme_support('custom-background', array(
+    // Add Support for Custom Backgrounds - Uncomment below if you're going to use
+    /*add_theme_support('custom-background', array(
 	'default-color' => 'FFF',
 	'default-image' => get_template_directory_uri() . '/img/bg.jpg'
-    ));
+    ));*/
 
-    // Add Support for Custom Header
-    add_theme_support('custom-header', array(
+    // Add Support for Custom Header - Uncomment below if you're going to use
+    /*add_theme_support('custom-header', array(
 	'default-image'			=> get_template_directory_uri() . '/img/headers/default.jpg',
 	'header-text'			=> false,
 	'default-text-color'		=> '000',
@@ -53,7 +53,7 @@ if (function_exists('add_theme_support'))
 	'wp-head-callback'		=> $wphead_cb,
 	'admin-head-callback'		=> $adminhead_cb,
 	'admin-preview-callback'	=> $adminpreview_cb
-    ));
+    ));*/
 
     // Enables post and comment RSS feed links to head
     add_theme_support('automatic-feed-links');
