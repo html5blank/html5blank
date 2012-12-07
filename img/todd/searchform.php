49077ba2d1a5ccf7b1caf0b83d87a42ae16630bb
Todd Motto <todd@asabell.co.uk>
2012-12-07 15:36:34 +0000

    Amend

--- img/todd/searchform.php
+++
@@ -1,7 +0,0 @@
-<!-- Searchform -->
-<form method="get" class="search item" action="<?php echo home_url(); ?>" >
-	<input id="s" type="text" name="s" onfocus="if(this.value==''){this.value=''};" 
-	onblur="if(this.value==''){this.value=''};" value="">
-	<input class="searchsubmit" type="submit" value="<?php _e( 'Search', 'html5blank' ); ?>">
-</form>
-<!-- /Searchform -->
