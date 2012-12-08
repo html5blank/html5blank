74553b6ff79982d502319bb268d62bcb8d7ffaa7
toddmotto <create@toddmotto.com>
2012-12-08 10:42:45 +0000

    Fixed indented space

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(function() {
 	
 	// Start using SVG!
 	// http://www.toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script
-    if(!Modernizr.svg) {
+    	if(!Modernizr.svg) {
 	    $('img[src*="svg"]').attr('src', function () {
 	        return $(this).attr('src').replace('.svg', '.png');
 	    });
