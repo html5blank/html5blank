a1ee4ff9133f1397a9229e840db85282354b1cf7
Todd Motto <create@toddmotto.com>
2012-12-09 12:03:07 +0000

    SVG and PNG Modernizr detect and script support

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 // DOM Ready
-$(document).ready(function() {
-	// jQuery Code
+$(function() {
 	
-	
-	
+	// SVG Modernizr detect and PNG replace
+	// toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script
+	if(!Modernizr.svg) {
+	    $('img[src*="svg"]').attr('src', function () {
+	        return $(this).attr('src').replace('.svg', '.png');
+	    });
+	}
 	
 	// Responsive Projects, iPhone/iPad URL bar hides itself on pageload
 	if (navigator.userAgent.indexOf('iPhone') != -1) {
