07f8ed9e98c9e574e60785cabe9228ed9932454a
Todd Motto <create@toddmotto.com>
2012-12-27 22:59:15 +0000

    Custom SVG feature detection script, Modernizr and jQuery free

--- js/scripts.js
+++ js/scripts.js
@@ -14,12 +14,20 @@ $(function() {
 		}
 	});
 	
-	// SVG Modernizr detect and PNG replace
-	// toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script
-	if(!Modernizr.svg) {
-	    $('img[src*="svg"]').attr('src', function () {
-	        return $(this).attr('src').replace('.svg', '.png');
-	    });
+	// SVG custom feature detection and svg to png fallback
+	// toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script#update
+	function supportsSVG() {
+		return !! document.createElementNS && !! document.createElementNS('http://www.w3.org/2000/svg','svg').createSVGRect;	
+	}
+	if (!supportsSVG()) {
+		document.documentElement.className = ' no-svg';
+		var imgs = document.getElementsByTagName('img'),
+			dotSVG = /.*\.svg$/;
+		for(var i = 0; i != imgs.length; ++i) {
+			if(imgs[i].src.match(dotSVG)) {
+				imgs[i].src = imgs[i].src.slice(0, -3) + "png";
+			}
+		}
 	}
 	
 	// iPhone Safari URL bar hides itself on pageload
