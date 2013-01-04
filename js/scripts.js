1789afc7a91a33d7da5d49fbc72df51a2d27eca8
toddmotto <create@toddmotto.com>
2013-01-04 15:05:56 +0000

    Remove iPhone scrollbar onload script
    Sometimes jilts the page back to top if you've already scrolled down. Not a necessary feature.

--- js/scripts.js
+++ js/scripts.js
@@ -31,16 +31,5 @@ $(function() {
 			}
 		}
 	}
-	
-	// iPhone Safari URL bar hides itself on pageload
-	if (navigator.userAgent.indexOf('iPhone') != -1) {
-	    addEventListener("load", function () {
-	        setTimeout(hideURLbar, 0);
-	    }, false);
-	}
-	
-	function hideURLbar() {
-	    window.scrollTo(0, 0);
-	}
-	
+
 });
