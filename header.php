5517dcc29534152bd3ded40cecd9a0e4fad359b7
Todd Motto <create@toddmotto.com>
2013-02-03 12:03:32 +0000

    2.0.1, Conditionizr raw JS

--- header.php
+++ header.php
@@ -1,8 +1,5 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
-<!--[if IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
-<!--[if IE 8]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
-<!--[if gt IE 8]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]--> 
+<html <?php language_attributes(); ?> class="no-js">
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,28 @@
 // DOM Ready
 $(function() {
+
+	// Conditionizr, legacy, retina script and style loader
+	// conditionizr.com
+	conditionizr({
+		debug      : true,
+		scriptSrc  : 'js/conditionizr/',
+		styleSrc   : 'css/conditionizr/',
+		ieLessThan : { active: true, version: '9', scripts: true, styles: true, classes: true, customScript: 'none'},
+		chrome     : { scripts: true, styles: true, classes: true, customScript: 'none' },
+		safari     : { scripts: true, styles: true, classes: true, customScript: 'none' },
+		opera      : { scripts: true, styles: true, classes: true, customScript: 'none' },
+		firefox    : { scripts: true, styles: true, classes: true, customScript: 'none' },
+		ie10       : { scripts: true, styles: true, classes: true, customScript: 'none' },
+		ie9        : { scripts: true, styles: true, classes: true, customScript: 'none' },
+		ie8        : { scripts: true, styles: true, classes: true, customScript: 'none' },
+		ie7        : { scripts: true, styles: true, classes: true, customScript: 'none' },
+		ie6        : { scripts: true, styles: true, classes: true, customScript: 'none' },
+		retina     : { scripts: true, styles: true, classes: true, customScript: 'none' },
+		mac    : true,
+		win    : true,
+		x11    : true,
+		linux  : true
+	});
 	
 	// SVG custom feature detection and svg to png fallback
 	// toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script#update
