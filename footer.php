f635dd18af1b170197f33b2c0da6ce23966b4c0c
Todd Motto <todd@toddmotto.com>
2013-12-02 10:12:23 +0000

    Update to universal Google Analytics

--- footer.php
+++ footer.php
@@ -18,10 +18,12 @@
 		
 		<!-- analytics -->
 		<script>
-			var _gaq=[['_setAccount','UA-XXXXXXXX-XX'],['_trackPageview']];
-			(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-			g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-			s.parentNode.insertBefore(g,s)})(document,'script');
+		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
+		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
+		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
+		ga('send', 'pageview');
 		</script>
 	
 	</body>
