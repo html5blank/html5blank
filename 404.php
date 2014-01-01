1433fe67080a8ecb054bfb235d4ecc803d0670be
James Hobson <james@floq.co.uk>
2014-01-01 16:01:46 +0200

    404 page doesn’t have an ID or class for the article - suggest hardcode

--- 404.php
+++ 404.php
@@ -2,21 +2,21 @@
 
 	<!-- section -->
 	<section role="main">
-	
+
 		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		
+		<article id="post-404">
+
 			<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
 			<h2>
 				<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
 			</h2>
-			
+
 		</article>
 		<!-- /article -->
-		
+
 	</section>
 	<!-- /section -->
-	
+
 <?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
