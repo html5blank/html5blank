bd29ce70d9f73e5576cf1f5dff7344eb790f7f71
Warface <carl.spooner@videotron.ca>
2014-02-13 09:31:36 -0500

    Update header.php
    Fixed the viewport meta to work on iPhone. Separate parameters with ; instead of ,

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+		<meta name="viewport" content="width=device-width;initial-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 
 		<?php wp_head(); ?>
