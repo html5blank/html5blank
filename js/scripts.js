11771550aeff17d7b6d69ad321802b813ab6a1d1
Todd Motto <todd@toddmotto.com>
2014-03-24 18:56:23 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,11 @@
-// DOM Ready
-jQuery(document).ready(function($) {
-
-	// SVG fallback
-	// toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script#update
-	if (!Modernizr.svg) {
-		var imgs = document.getElementsByTagName('img');
-		for (var i = 0; i < imgs.length; i++) {
-			if (/.*\.svg$/.test(imgs[i].src)) {
-				imgs[i].src = imgs[i].src.slice(0, -3) + 'png';
-			}
-		}
-	}
-
-});
+(function ($, root, undefined) {
+	
+	$(function () {
+		
+		'use strict';
+		
+		// DOM ready, take it away
+		
+	});
+	
+})(jQuery, this);
