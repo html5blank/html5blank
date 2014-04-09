91825d02d8ab1305e6fd47326756ab9c05deef03
Fabián Núñez <fabian@forkhq.com>
2014-04-08 22:39:25 -0300

    Improved Spanish translation + es_CL
    Added the searchform.php placeholder to the POT file, and translated the
    string to Spanish. Also added consistency, proper translations for all
    strings and es_CL, since it's my WP language. Feel free to delete those
    files if you wish, since es_ES are the same.

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <!-- search -->
 <form class="search" method="get" action="<?php echo home_url(); ?>" role="search">
-	<input class="search-input" type="search" name="s" placeholder="To search, type and hit enter.">
+	<input class="search-input" type="search" name="s" placeholder="<?php _e( 'To search, type and hit enter.', 'html5blank' ); ?>">
 	<button class="search-submit" type="submit" role="button"><?php _e( 'Search', 'html5blank' ); ?></button>
 </form>
 <!-- /search -->
