64e5f61ec22c8959c80a8a71b08c432c0097a6ce
ccurtin <git@christopherjamescurtin.com>
2014-11-21 09:54:53 -0500

    replace title attribute with correct Wordpress tag

--- src/loop.php
+++ src/loop.php
@@ -5,7 +5,7 @@
 
 		<!-- post thumbnail -->
 		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
 				<?php the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
 			</a>
 		<?php endif; ?>
@@ -13,7 +13,7 @@
 
 		<!-- post title -->
 		<h2>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
 		</h2>
 		<!-- /post title -->
 
