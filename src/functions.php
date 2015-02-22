509c8a6797a48aa463dfb52f04a8b3e3df9031e8
Kuba Niewiarowski <marsjaninzmarsa@gmail.com>
2015-02-22 22:36:30 +0100

    deleted add_theme_support('menus') due to notes bellow.
    http://codex.wordpress.org/add_theme_support#Notes

--- src/functions.php
+++ src/functions.php
@@ -24,8 +24,6 @@ if (!isset($content_width))
 
 if (function_exists('add_theme_support'))
 {
-    // Add Menu Support
-    add_theme_support('menus');
 
     // Add Thumbnail Theme Support
     add_theme_support('post-thumbnails');
