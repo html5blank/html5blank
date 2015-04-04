5f08b717471a7a48b50787f11256901f889fe318
WebNinjrDesign <tim@webninjrdesign.com>
2015-04-05 08:01:36 +1000

    escaped src urls
    According to Wordpress all src URLs should be escaped in all situations as best practice

--- src/header.php
+++ src/header.php
@@ -5,8 +5,8 @@
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' : '; } ?><?php bloginfo('name'); ?></title>
 
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+        <link href="<?php echo echo esc_url(get_template_directory_uri()); ?>/img/icons/favicon.ico" rel="shortcut icon">
+        <link href="<?php echo echo esc_url(get_template_directory_uri()); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,7 +17,7 @@
         // conditionizr.com
         // configure environment tests
         conditionizr.config({
-            assets: '<?php echo get_template_directory_uri(); ?>',
+            assets: '<?php echo echo esc_url(get_template_directory_uri()); ?>',
             tests: {}
         });
         </script>
@@ -35,7 +35,7 @@
 					<div class="logo">
 						<a href="<?php echo home_url(); ?>">
 							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
+							<img src="<?php echo echo esc_url(get_template_directory_uri()); ?>/img/logo.svg" alt="Logo" class="logo-img">
 						</a>
 					</div>
 					<!-- /logo -->
