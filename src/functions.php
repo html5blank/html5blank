7aa36dfa31ea74b5a25b6f0d9df5a644643c9c92
Kirill Kliavin <kirill@studiothick.com>
2015-07-11 19:29:34 +1000

    move prod scripts to footer

--- src/functions.php
+++ src/functions.php
@@ -118,7 +118,7 @@ function html5blank_header_scripts()
         // If production
         } else {
             // Scripts minify
-            wp_register_script('html5blankscripts-min', get_template_directory_uri() . '/js/scripts.min.js', array(), '1.0.0');
+            wp_register_script('html5blankscripts-min', get_template_directory_uri() . '/js/scripts.min.js', array(), '1.0.0', true);
             // Enqueue Scripts
             wp_enqueue_script('html5blankscripts-min', get_template_directory_uri() . '/js/scripts.min.js', array(), '1.0.0', true);
         }
