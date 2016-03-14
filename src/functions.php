9c3570136e8ba416412cd2681a24907ffab73ff0
Dzakus <Dzakus13@gmail.com>
2016-03-14 18:55:33 +0100

    Add title-tag support

--- src/functions.php
+++ src/functions.php
@@ -53,6 +53,7 @@ if (function_exists('add_theme_support'))
 
     // Enables post and comment RSS feed links to head
     add_theme_support('automatic-feed-links');
+    add_theme_support('title-tag');
 
     // Localisation Support
     load_theme_textdomain('html5blank', get_template_directory() . '/languages');
