642db530af67e50dd448a57e71d2731c0c5104c4
Lou Regla <lou.regla@gofullscale.com>
2017-10-19 20:55:37 -0700

    modified webpack configuration to deploy code to 'dist' directory consistent with previous gulp configuration

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 module.exports = {
   entry: ['./src/js/scripts.js', './src/css/sass/style.scss'],
   output: {
-    path: path.resolve(__dirname, './src/js/'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, './dist/js/'),
+    filename: 'scripts.min.js'
   },
   module: {
     rules: [
@@ -32,7 +32,7 @@ module.exports = {
     ]
   },
   plugins: [
-    new ExtractTextPlugin('../css/style.css'),
+    new ExtractTextPlugin('../style.css'),
     new UglifyJSPlugin(),
     new webpack.HotModuleReplacementPlugin()
   ]
