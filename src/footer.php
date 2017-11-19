9c875120b2f179305d053c2bb7617e26c1a47c0a
Devio Digital <deviodigital@gmail.com>
2017-11-18 20:45:13 -0500

    Make WordPress link open in new window

--- src/footer.php
+++ src/footer.php
@@ -4,7 +4,7 @@
 				<!-- copyright -->
 				<p class="copyright">
 					&copy; <?php echo esc_html( date( 'Y' ) ); ?> Copyright <?php bloginfo( 'name' ); ?>. <?php esc_html_e( 'Powered by', 'html5blank' ); ?>
-					<a href="//wordpress.org">WordPress</a> &amp; <a href="//html5blank.com" target="_blank">HTML5 Blank</a>.
+					<a href="//wordpress.org" target="_blank">WordPress</a> &amp; <a href="//html5blank.com" target="_blank">HTML5 Blank</a>.
 				</p>
 				<!-- /copyright -->
 
