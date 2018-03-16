8095f5ab8f27529c4ee45d8689bc982cd7f34d62
Marko Šustić <marko.sustic@rimac-automobili.com>
2018-03-16 11:07:47 +0100

    Some minor bug fixes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,8 +4,7 @@
 	$( function() {
 
 		// DOM ready, take it away
-		/*var m = new Marka( '#menu_icon' );
-		m.set( 'bars' ).size( 40 );*/
-		console.log( 'tlalala' );
+		var m = new Marka( '#menu_icon' );
+		m.set( 'bars' ).size( 40 );
 	});
 } ( this, jQuery ) );
